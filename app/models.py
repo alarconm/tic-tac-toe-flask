52895a73645fa3be2765f0affea3d109865b9c07
Michael Alarcon <malarconii@yahoo.com>
2017-06-18 16:28:47 -0700

    Fully functional game with win condition detection and restarting of game when user decides to do so.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 game_board = {'x1': 'X', 'x2': 'X', 'x3': 'X', 'x4': 'X', 'x5': 'X', 'x6': 'X', 'x7': 'X', 'x8': 'X', 'x9': 'X'}
-moves_made = 0
+moves_made = 1
 
 
 def game(position, player):
@@ -11,3 +11,53 @@ def player_select():
     if moves_made % 2 == 0:
         return 'A'
     return 'B'
+
+
+def winner():
+    if game_board['x1'] == 'A' and game_board['x2'] == 'A' and game_board['x3'] == 'A':
+        return 'winner'
+
+    if game_board['x4'] == 'A' and game_board['x5'] == 'A' and game_board['x6'] == 'A':
+        return 'winner'
+
+    if game_board['x7'] == 'A' and game_board['x8'] == 'A' and game_board['x9'] == 'A':
+        return 'winner'
+
+    if game_board['x1'] == 'A' and game_board['x4'] == 'A' and game_board['x7'] == 'A':
+        return 'winner'
+
+    if game_board['x2'] == 'A' and game_board['x5'] == 'A' and game_board['x8'] == 'A':
+        return 'winner'
+
+    if game_board['x3'] == 'A' and game_board['x6'] == 'A' and game_board['x9'] == 'A':
+        return 'winner'
+
+    if game_board['x1'] == 'A' and game_board['x5'] == 'A' and game_board['x9'] == 'A':
+        return 'winner'
+
+    if game_board['x7'] == 'A' and game_board['x5'] == 'A' and game_board['x3'] == 'A':
+        return 'winner'
+
+    if game_board['x1'] == 'B' and game_board['x2'] == 'B' and game_board['x3'] == 'B':
+        return 'winner'
+
+    if game_board['x4'] == 'B' and game_board['x5'] == 'B' and game_board['x6'] == 'B':
+        return 'winner'
+
+    if game_board['x7'] == 'B' and game_board['x8'] == 'B' and game_board['x9'] == 'B':
+        return 'winner'
+
+    if game_board['x1'] == 'B' and game_board['x4'] == 'B' and game_board['x7'] == 'B':
+        return 'winner'
+
+    if game_board['x2'] == 'B' and game_board['x5'] == 'B' and game_board['x8'] == 'B':
+        return 'winner'
+
+    if game_board['x3'] == 'B' and game_board['x6'] == 'B' and game_board['x9'] == 'B':
+        return 'winner'
+
+    if game_board['x1'] == 'B' and game_board['x5'] == 'B' and game_board['x9'] == 'B':
+        return 'winner'
+
+    if game_board['x7'] == 'B' and game_board['x5'] == 'B' and game_board['x3'] == 'B':
+        return 'winner'
--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 from app import app
 from app import models
-
 
 
 @app.route('/')
@@ -23,9 +22,31 @@ def index():
 @app.route('/game/move')
 def game():
 
+    if request.args.get('start'):
+        models.game_board = {'x1': 'X', 'x2': 'X', 'x3': 'X', 'x4': 'X', 'x5': 'X', 'x6': 'X', 'x7': 'X', 'x8': 'X', 'x9': 'X'}
+        models.moves_made = -1
+        return redirect('/game/move')
+
+
     player = models.player_select()
 
     models.game_board[request.args.get('move')] = player
+
+
+    # if models.moves_made > 9: TODO: create stalemate page, or flash then index?
+
+    if models.winner() == 'winner':
+        return render_template('winner.html', winner=player,
+                               x1=models.game_board['x1'],
+                               x2=models.game_board['x2'],
+                               x3=models.game_board['x3'],
+                               x4=models.game_board['x4'],
+                               x5=models.game_board['x5'],
+                               x6=models.game_board['x6'],
+                               x7=models.game_board['x7'],
+                               x8=models.game_board['x8'],
+                               x9=models.game_board['x9'],)
+
     models.moves_made += 1
     player = models.player_select()
 
