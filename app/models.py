abb0f2710a23368c3719b229260852e20ab6f17a
Michael Alarcon <malarconii@yahoo.com>
2017-06-18 11:20:11 -0700

    All buttons working, Player toggle working.

--- app/models.py
+++ app/models.py
@@ -1,15 +1,13 @@
 game_board = {'x1': 'X', 'x2': 'X', 'x3': 'X', 'x4': 'X', 'x5': 'X', 'x6': 'X', 'x7': 'X', 'x8': 'X', 'x9': 'X'}
+moves_made = 0
+
 
 def game(position, player):
     '''Takes in key/value pair, then updates the game board'''
 
     game_board[position] = player
 
-
-def players_toggle(current_player):
-    '''Pass in which player just finished their move as either 'A' or 'B'
-    return the next player'''
-
-    if current_player == 'A':
-        return 'B'
-    return 'A'
+def player_select():
+    if moves_made % 2 == 0:
+        return 'A'
+    return 'B'
--- app/views.py
+++ app/views.py
@@ -19,10 +19,17 @@ def index():
                            x8=models.game_board['x8'],
                            x9=models.game_board['x9'])
 
-@app.route('/x1')
-def x1():
-    player = request.form['player']
-    models.game_board['x1'] = player
+
+@app.route('/game/move')
+def game():
+
+    player = models.player_select()
+
+    models.game_board[request.args.get('move')] = player
+    models.moves_made += 1
+    player = models.player_select()
+
+
 
     return render_template('game.html', title='Tic-Tac-Toe',
                            x1=models.game_board['x1'],
@@ -33,6 +40,6 @@ def x1():
                            x6=models.game_board['x6'],
                            x7=models.game_board['x7'],
                            x8=models.game_board['x8'],
-                           x9=models.game_board['x9'])
+                           x9=models.game_board['x9'], current_player=player)
 
     
