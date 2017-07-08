84ce337e9e30281d265490bb43f13cbf129d5181
Michael Alarcon <malarconii@yahoo.com>
2017-07-07 20:37:31 -0700

    slight refactoring of returning templates

--- app/controller.py
+++ app/controller.py
@@ -7,16 +7,7 @@ from app import models
 @app.route('/index')
 def index():
 
-    return render_template('index.html', title="Tic-Tac-Toe",
-                           x1=models.game_board['x1'],
-                           x2=models.game_board['x2'],
-                           x3=models.game_board['x3'],
-                           x4=models.game_board['x4'],
-                           x5=models.game_board['x5'],
-                           x6=models.game_board['x6'],
-                           x7=models.game_board['x7'],
-                           x8=models.game_board['x8'],
-                           x9=models.game_board['x9'])
+    return render_template('index.html', title="Tic-Tac-Toe")
 
 
 @app.route('/game/move')
@@ -26,7 +17,6 @@ def game():
         models.game_board = {'x1': 'X', 'x2': 'X', 'x3': 'X', 'x4': 'X', 'x5': 'X', 'x6': 'X', 'x7': 'X', 'x8': 'X', 'x9': 'X'}
         models.moves_made = -1
         return redirect('/game/move')
-
 
     player = models.player_select()
 
--- app/models.py
+++ app/models.py
@@ -1,11 +1,7 @@
+new_game_board = {'x1': 'X', 'x2': 'X', 'x3': 'X', 'x4': 'X', 'x5': 'X', 'x6': 'X', 'x7': 'X', 'x8': 'X', 'x9': 'X'}
 game_board = {'x1': 'X', 'x2': 'X', 'x3': 'X', 'x4': 'X', 'x5': 'X', 'x6': 'X', 'x7': 'X', 'x8': 'X', 'x9': 'X'}
 moves_made = 1
 
-
-def game(position, player):
-    '''Takes in key/value pair, then updates the game board'''
-
-    game_board[position] = player
 
 def player_select():
     if moves_made % 2 == 0:
